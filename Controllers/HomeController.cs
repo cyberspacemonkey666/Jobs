d48f7903d1224e13f54c85262afd0f60516449df
cyberspacemonkey666 <tannyamakiri@gmail.com>
2020-04-27 12:58:37 +0100

    project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿using JobPortal.Engine;
+using JobPortal.ViewModel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        AmakiriEntities2 db = new AmakiriEntities2();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveJ(string email,string jt,string jl,string jr,string jty,string jd,string cn,string cd,string cw)
+        {
+            JobPost jp = new JobPost();
+            jp.CompanyDescription = jd;
+            jp.CompanyEmail=email;
+            jp.CompanyLogo="Logo";
+            jp.CompanyName=cn;
+            jp.CompanyWebsite=cw;
+            jp.JobDescription=jd;
+            jp.JobTitle=jt;
+            jp.JobType=jty;
+            jp.Location=jl;
+            jp.Region=jr;
+            db.JobPosts.Add(jp);
+            var k = db.SaveChanges();
+
+            if (k>0)
+            {
+                return Json(new { value = 1 });
+            }
+
+            return Json(new { value = 0});
+        }
+        public JsonResult SignUp(string email, string password, string phone)
+        {
+
+           // var check = db.Logins.Select(x => x.Email == email).SingleOrDefault();
+            var check = db.Logins.Select(x => x.Email == email).FirstOrDefault();
+            if (check)
+            {
+                return Json(new { value = 2 });
+            }
+
+            Login lg = new Login();
+
+            lg.Email = email;
+            lg.Password = password;
+            lg.Phone = phone;
+            db.Logins.Add(lg);
+            var k= db.SaveChanges();
+            if (k>0)
+            {
+               // EmailSender sendMail = new EmailSender("Dear Admin " + email + " Just sign up on portal","New Users",email );
+
+               // sendMail.Shooter();
+                return Json(new {value=1});
+            }
+
+
+            return Json(new {value=0});
+        }
+
+        [HttpPost]
+        public JsonResult Login(string email,string password)
+        {
+            var LogMeIn = db.Logins.Select(h => h.Email == email && h.Password == password).FirstOrDefault();
+            if (LogMeIn)
+            {
+                Session["mail"]=email;
+                return Json(new { value = 1 });
+
+            }
+
+            return Json(new { value = 0 });
+        }
+
+
+        public JsonResult GetAll()
+        {
+            var jblist = db.JobPosts.ToList();
+            var json = JsonConvert.SerializeObject(jblist);
+             return Json(new {msg= json });
+        }
+
+        public JsonResult Search(string JobType, string SearchLocation)
+        {
+          //  var searchresult = db.JobPosts.Select();
+
+            return Json(new { })
+;
+        }
+        public ActionResult Login()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult PostJob()
+        {
+
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Engine/EmailSender.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace JobPortal.Engine
+{
+
+    public class EmailSender
+    {
+        static string smtpAddress = "smtp.gmail.com";
+        static int portNumber = 465;//587;
+        static bool enableSSL = true;
+        static string emailFromAddress = "amakereproject@gmail.com"; //Sender Email Address  
+        static string password = "password123.pro"; //Sender Password  
+        static string emailToAddress = "kayceehans@gmail.com"; //Receiver Email Address  
+        //  static string subject = "Hello";  
+        //  static string body = "Hello, This is Email sending test using gmail.";  
+
+        static string Message, Subject, Email;
+        public EmailSender(string message, string subject, string email)
+        {
+            Message = message;
+            Subject = subject;
+            Email = email;
+        }
+        public void Shooter()
+        {
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(emailFromAddress);
+                mail.To.Add(emailToAddress);
+                mail.Subject = Subject;
+                mail.Body = Message;
+                mail.IsBodyHtml = false;
+                //mail.Attachments.Add(new Attachment("D:\\TestFile.txt"));//--Uncomment this to send any attachment  
+                using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.Credentials = new NetworkCredential(emailFromAddress, password);
+                   // smtp.UseDefaultCredentials = true;
+                    
+                    smtp.UseDefaultCredentials = false;
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                    //var client = new SmtpClient("smtp.gmail.com", 587)
+                    //{
+                    //    Credentials = new NetworkCredential("amakereproject@gmail.com", "password123.pro"),
+                    //    EnableSsl = true
+                    //    //EnableSSLOnMail=true
+                    //};
+                    ////client.Send("myusername@gmail.com", "myusername@gmail.com", "test", "testbody");
+                    //client.Send(Email, "kayceehans@gmail.com", Subject, Message);
+                }
+
+
+            }
+        }
+    }
+}
